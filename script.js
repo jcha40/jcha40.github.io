54d1919cae47b62d8fcebc8eb8983c508b751f0b
jcha40 <sungsoocha97@gmail.com>
2022-08-24 12:40:14 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//scroll to view smoothly
+document.querySelector(".navbar-nav").addEventListener("click", function (e) {
+  //   console.log("clicked");
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//reveal section
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
